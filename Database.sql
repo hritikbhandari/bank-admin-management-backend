7107ac96cd842cc062dc0d0f4f551d0dc17e9667
gautamdevloper <65808225+gautamdevloper@users.noreply.github.com>
2022-11-03 16:28:08 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,9 +1,11 @@
-
+drop database bank_admin;
 
 create database bank_admin;
 
+use bank_admin;
+
 create table customer_master(
-customer_number VARCHAR(6),
+customer_number VARCHAR(6) PRIMARY KEY,
 firstname VARCHAR(30),
 middlename VARCHAR(30),
 lastname VARCHAR(30),
@@ -28,4 +30,22 @@ create table account_master(
 	account_status VARCHAR(10),
 	FOREIGN KEY (customer_number) REFERENCES customer_master (customer_number),
 	FOREIGN KEY (branch_id) REFERENCES branch_master(branch_id)
+);
+
+create table loan_details(
+customer_number VARCHAR(6),
+branch_id VARCHAR(6),
+loan_amount INT(7),
+PRIMARY KEY(customer_number,branch_id)
+);
+
+
+create table transaction_details(
+transaction_number varchar(6) PRIMARY KEY,
+account_number varchar(6),
+date_of_transaction DATE,
+medium_of_transaction varchar(20),
+transaction_type varchar(20),
+transaction_amount INT,
+FOREIGN KEY (account_number) REFERENCES account_master (account_number)
 );
