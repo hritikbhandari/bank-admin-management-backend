df71a8fe75df60b365a217e1464722b9d6123eb7
gautamdevloper <65808225+gautamdevloper@users.noreply.github.com>
2022-11-03 16:01:28 +0530

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,13 @@
+
+
+create database bank_admin;
+
+create table customer_master(
+customer_number VARCHAR(6),
+firstname VARCHAR(30),
+middlename VARCHAR(30),
+lastname VARCHAR(30),
+customer_city VARCHAR(15),
+customer_contact_no varchar(10),
+occupation varchar(20),
+customer_date_of_birth date);
