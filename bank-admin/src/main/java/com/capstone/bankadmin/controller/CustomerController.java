cc0c2c0a3624410e931ffc7a1c22c3a5ade5377e
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-11-07 14:46:06 +0530

    Create CustomerController.java

---
+++ bank-admin/src/main/java/com/capstone/bankadmin/controller/CustomerController.java
@@ -0,0 +1,29 @@
+package com.capstone.bankadmin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capstone.bankadmin.service.CustomerService;
+
+import antlr.collections.List;
+
+@RestController
+public class CustomerController {
+
+	@Autowired
+	CustomerService service;
+	@GetMapping("/all")
+	public List<Customer> showAllData(){
+		return service.getAllData();
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public String deletePost(@PathVariable("id")int id){
+		service.deleteCustomer(id);
+		
+		return "data deleted successfully..";
+	}
+}
