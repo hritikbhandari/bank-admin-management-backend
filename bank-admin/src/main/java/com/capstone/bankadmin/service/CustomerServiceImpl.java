5c6411cce5f0f6359281db731471a0124278f08e
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-11-08 13:44:12 +0530

    Update CustomerServiceImpl.java

--- bank-admin/src/main/java/com/capstone/bankadmin/service/CustomerServiceImpl.java
+++ bank-admin/src/main/java/com/capstone/bankadmin/service/CustomerServiceImpl.java
@@ -3,8 +3,12 @@ package com.capstone.bankadmin.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.capstone.bankadmin.exception.NotFoundException;
 import com.capstone.bankadmin.model.Customer;
 import com.capstone.bankadmin.repository.CustomerRepository;
 
@@ -21,10 +25,20 @@ public class CustomerServiceImpl implements CustomerService{
 	}
 
 	@Override
-	public void deleteCustomer(int id) {
-		// TODO Auto-generated method stub
-		repo.deleteById(id);
+	public String deleteCustomer(int id) throws NotFoundException{
 		
+		// TODO Auto-generated method stub
+		String  customerId=String.valueOf(id);
+		Customer c=repo.findById(id).orElseThrow(()->new NotFoundException("Customer with Id: "+customerId+" not found"));
+		repo.deleteById(c.getCustomerNumber());
+		return (String)("customer with id: "+customerId+" deleted!"); 
+		
+	}
+	
+	@ExceptionHandler(value = NotFoundException.class)
+	public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+		System.out.println(ex.getMessage());
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 }
