2eaaffc01eeae4f6d70721c769945ed7cc3e37a1
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-11-08 13:42:56 +0530

    Update CustomerService.java

--- bank-admin/src/main/java/com/capstone/bankadmin/service/CustomerService.java
+++ bank-admin/src/main/java/com/capstone/bankadmin/service/CustomerService.java
@@ -1,10 +1,11 @@
 package com.capstone.bankadmin.service;
 import java.util.List;
 
+import com.capstone.bankadmin.exception.NotFoundException;
 import com.capstone.bankadmin.model.Customer;
 
 
 public interface CustomerService {
        public List<Customer>getAllData();
-       public void deleteCustomer(int id);
+       public String deleteCustomer(int id) throws NotFoundException;
 }
