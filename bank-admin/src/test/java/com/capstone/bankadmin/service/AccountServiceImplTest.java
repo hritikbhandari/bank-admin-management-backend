b6755dc6582c22975e0820ae750f96bb175ccfb7
EashwarPrabu <eashwarpc@gmail.com>
2022-11-09 22:58:15 +0530

    Fix for account service test

--- bank-admin/src/test/java/com/capstone/bankadmin/service/AccountServiceImplTest.java
+++ bank-admin/src/test/java/com/capstone/bankadmin/service/AccountServiceImplTest.java
@@ -1,10 +1,11 @@
-package com.capstone.bankadmin.account;
+package com.capstone.bankadmin.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.sql.Date;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,7 +21,7 @@ import com.capstone.bankadmin.model.Account;
 import com.capstone.bankadmin.repository.AccountRepository;
 import com.capstone.bankadmin.service.AccountServiceImpl;
 @SpringBootTest
-public class accountServiceImplTest {
+public class AccountServiceImplTest {
 
 	 @Mock
 	 private AccountRepository repo;
@@ -32,12 +33,12 @@ public class accountServiceImplTest {
 	 public void getAllAccountDetailsTest() {
 		 
 		 when(repo.findAll()).thenReturn(
-				 Stream.of( new Account(12,"1",5000,"Saving","Active"),new Account(12,"1",15000,"Saving","Inactive")).collect(Collectors.toList()));
+				 Stream.of( new Account(12,"1",5000,"Saving","Active", new Date(2022, 11, 9)),new Account(12,"1",15000,"Saving","Inactive", new Date(2022, 11, 9))).collect(Collectors.toList()));
 		 assertEquals(2,service.getAllAccountDetails().size());
 	 }
 	 @Test
 	 public void deleteAccountTest() {
-		   Account account= new Account(12,"1",5000,"Saving","Active");
+		   Account account= new Account(12,"1",5000,"Saving","Active", new Date(2022, 11, 9));
 		   when(repo.findById(account.getAccountNumber())).thenReturn(Optional.of(account));
 	      String response=service.deleteAccount(account.getAccountNumber());
 		   assertEquals(response,"Branch with Id: " + "12" + " deleted!");
