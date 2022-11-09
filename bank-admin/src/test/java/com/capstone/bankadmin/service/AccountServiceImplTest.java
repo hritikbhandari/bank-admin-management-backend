59aec6ad2e5e20a413600945a1f1f60451572f08
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-11-09 21:04:01 +0530

    Create AccountServiceImplTest.java

---
+++ bank-admin/src/test/java/com/capstone/bankadmin/service/AccountServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.capstone.bankadmin.account;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.capstone.bankadmin.model.Account;
+import com.capstone.bankadmin.repository.AccountRepository;
+import com.capstone.bankadmin.service.AccountServiceImpl;
+@SpringBootTest
+public class accountServiceImplTest {
+
+	 @Mock
+	 private AccountRepository repo;
+	 @InjectMocks
+	 private AccountServiceImpl service;
+	 
+	
+	 @Test
+	 public void getAllAccountDetailsTest() {
+		 
+		 when(repo.findAll()).thenReturn(
+				 Stream.of( new Account(12,"1",5000,"Saving","Active"),new Account(12,"1",15000,"Saving","Inactive")).collect(Collectors.toList()));
+		 assertEquals(2,service.getAllAccountDetails().size());
+	 }
+	 @Test
+	 public void deleteAccountTest() {
+		   Account account= new Account(12,"1",5000,"Saving","Active");
+		   when(repo.findById(account.getAccountNumber())).thenReturn(Optional.of(account));
+	      String response=service.deleteAccount(account.getAccountNumber());
+		   assertEquals(response,"Branch with Id: " + "12" + " deleted!");
+		   
+	 }
+	 
+}
